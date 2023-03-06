6a258ab4087d5e09af103f622f4064fff6ee2c0b
looser88 <98893135+looser88@users.noreply.github.com>
2023-03-06 23:06:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/looser88/Mdiskcov-URLShort /URL-Shortener-V2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /URL-Shortener-V2
